78ba66b5cf7fa0f98fb3191f64fbc6d844a5719e
Irvin Raul Lopez <irvin.lopez@autofact.com.mx>
2024-08-01 14:00:12 -0600

    add userController and endpoint for signup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import express, { Request, Response } from 'express';
+const cors = require("cors");
+const userController = require('./controllers/usersController');
+
+const { CORS_ORIGIN } = require("../config");
+
+const corsOptions = {
+  origin: CORS_ORIGIN,
+  optionsSuccessStatus: 200
+}
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: true}));
+
+app.post('/signup', (req: Request, res: Response) => {
+  userController.signUp(req.body).then((response: any) => {
+    res.json(response);
+  }).catch((error: any) => {
+    res.json(JSON.stringify(error.sqlMessage || error));
+  });
 });
 
 app.listen(port, () => {
