51ca5b20288d491c8fd3bddfa9b5be25f80baa7c
Irvin Raul Lopez <irvin.lopez@autofact.com.mx>
2024-08-01 12:47:19 -0600

    add knex initial client

---
+++ config.ts
@@ -0,0 +1,11 @@
+const config =  {
+  DB_DATABASE: process.env.DB_DATABASE || "reportes_marsella",
+  DB_USER: process.env.DB_USER || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "",
+  DB_DIALECT: process.env.DB_DIALECT || "mysql2",
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_PORT: process.env.DB_PORT || 3306,
+  CORS_ORIGIN: process.env.CORS_ORIGIN || "*"
+}
+
+module.exports = config;
